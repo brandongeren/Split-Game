6ade1e89324dc6c3cb18142c7e283634b14ebb00
Brandon Geren <brandongeren@gmail.com>
2016-11-13 04:07:16 -0600

    Update UI

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveBall : MonoBehaviour {
 
 
-	public float Friction = 0.8f;
+	public float Friction = 0.9f;
 	public float Speed = 20f;
 	public float minSpeed = 0.5f;
 
@@ -14,6 +14,8 @@ public class MoveBall : MonoBehaviour {
 	void Update () {
 		transform.position = Vector3.MoveTowards(transform.position, 
 			transform.position + transform.forward, Speed * Time.deltaTime / transform.localScale.x);
+
+		print (transform.forward);
 		Speed = Speed * Friction;
 		if (Speed < minSpeed) {
 			Speed = 0;
