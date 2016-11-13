47c90d526a947c7bf8ee2713e84205bdc6f45ba4
Brandon Geren <brandongeren@gmail.com>
2016-11-13 02:53:30 -0600

    Added min speed

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -7,13 +7,15 @@ public class MoveBall : MonoBehaviour {
 
 	public float Friction = 0.8;
 	public float Speed = 20;
+	public float minSpeed = 0.5;
+
 
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.MoveTowards(transform.position, 
 			transform.position + transform.forward, Speed * Time.deltaTime / transform.localScale.x);
 		Speed = Speed * Friction;
-		if (Speed < 0.5) {
+		if (Speed < minSpeed) {
 			Speed = 0;
 		}
 	}
