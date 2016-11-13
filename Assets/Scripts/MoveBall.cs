ea499b7caf336ca296b00f6e093a315e29e708b2
Brandon Geren <brandongeren@gmail.com>
2016-11-13 02:46:41 -0600

    Added MoveBall script

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+
+	public float Friction = 0.8;
+	public float Speed = 20;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, 
+			transform.position + transform.forward, Speed * Time.deltaTime / transform.localScale.x);
+		Speed = Speed * Friction;
+	}
+}
---
+++ Assets/Scripts/Split.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split : MonoBehaviour {
+
+	void splitBy(int amount) {
+		
+	}
+
+}
